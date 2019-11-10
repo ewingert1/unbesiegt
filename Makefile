7630bc42c4c76d88cc092fa97ffeef6ef6e71731
Foltest <foltest@temeria.com>
2019-11-10 12:56:09 -0800

    ca marche toujours pas

--- Makefile
+++ Makefile
@@ -2,11 +2,13 @@
 CC = arm-none-eabi-gcc
 INC = -Iinclude -Iinclude/bsp
 SRC = src/*.c src/*.S src/bsp/rpi0w/*.c
-CLAGS = -c fmessage-length=0 -mcpu=arm1176jzf-s -mfpu=vfp
-LDFLAGS = -nostartfiles -T memmap.ld
+CLAGS = -c fmessage-length=0 -v
+RPI0FLAGS = -mfloat-abi=hard -mfpu=vfp -march=armv6zk -mtune=arm1176jzf-s
+RPI3FLAGS = -march=armv8-a -mfpu=neon-fp-armv8 -mtune=cortex-a53
+LDFLAGS = -nostartfiles -nostdlib -T memmap.ld
 
 kernel.elf :
-	$(CC) $(INC) $(CFLAGS) $(SRC) -o kernel.elf $(LDFLAGS)
+	$(CC) $(CFLAGS) $(RPI0FLAGS) $(INC) $(SRC) -o kernel.elf $(LDFLAGS)
 
 clean :
 	rm kernel*
--- src/notmain.c
+++ src/notmain.c
@@ -2,28 +2,30 @@
 #include "GPIO_Driver.h"
 
 
-#define SET_GPIO_PIN_IN(GPIO_N) (gpio_add[GPIO_N/10] &= ~(0b111<<(3*(GPIO_N%10))))
-#define SET_GPIO_PIN_OUT(GPIO_N) (gpio_add[GPIO_N/10] = (gpio_add[GPIO_N/10] & ~(0b111<<(3*(GPIO_N%10)))) | (1<<(3*(GPIO_N%10))))
-
-#define SET_GPIO_PIN_HIGH(GPIO_N) {gpio_add[0x1C/4+(GPIO_N/32)] = (1 << (GPIO_N%32) );}
-#define SET_GPIO_PIN_LOW(GPIO_N) {gpio_add[0x28/4+(GPIO_N/32)] = (1 << (GPIO_N%32) );}
-
-volatile uint32_t* gpio_add =  (uint32_t*)GPIO_ADDRESS;
+// #define SET_GPIO_PIN_IN(GPIO_N) (gpio_add[GPIO_N/10] &= ~(0b111<<(3*(GPIO_N%10))))
+// #define SET_GPIO_PIN_OUT(GPIO_N) (gpio_add[GPIO_N/10] = (gpio_add[GPIO_N/10] & ~(0b111<<(3*(GPIO_N%10)))) | (1<<(3*(GPIO_N%10))))
+//
+// #define SET_GPIO_PIN_HIGH(GPIO_N) {gpio_add[0x1C/4+(GPIO_N/32)] = (1 << (GPIO_N%32) );}
+// #define SET_GPIO_PIN_LOW(GPIO_N) {gpio_add[0x28/4+(GPIO_N/32)] = (1 << (GPIO_N%32) );}
+//
+// #define GPIO_ADDRESS 0x20200000
+// volatile uint32_t* gpio_add =  (uint32_t*)GPIO_ADDRESS;
 
 
 int  notmain()
 {
-	SET_GPIO_PIN_OUT(21);
-	SET_GPIO_PIN_HIGH(21);
-// //	gpio_driver_t *gpio_drv = create_gpio_driver();
-// 	pin_info_t info;
-// 	info.pin_number = 21;
-// 	info.pin_behaviour = GPIO_OUTPUT;
-// 	info.pin_level = GPIO_HIGH;
-// //	gpio_drv->set_pin_behaviour(&info, NULL);
-// //	gpio_drv->set_pin_level(&info, NULL);
-// 	rpi0w_set_pin_behaviour(&info,NULL);
-// 	rpi0w_set_pin_level(&info,NULL);
+
+	// SET_GPIO_PIN_OUT(26);
+	// SET_GPIO_PIN_HIGH(26);
+//	gpio_driver_t *gpio_drv = create_gpio_driver();
+	pin_info_t info;
+	info.pin_number = 26;
+	info.pin_behaviour = GPIO_OUTPUT;
+	info.pin_level = GPIO_HIGH;
+//	gpio_drv->set_pin_behaviour(&info, NULL);
+//	gpio_drv->set_pin_level(&info, NULL);
+	rpi0w_set_pin_behaviour(&info,NULL);
+	rpi0w_set_pin_level(&info,NULL);
 	while(1);
 	return 1;
 }
